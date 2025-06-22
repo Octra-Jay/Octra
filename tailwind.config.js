f8965de9dc6074a0b38dce5239d5cf74da4f05cb
Jayden Wopperer <jayden_octra@yahoo.com>
2025-06-22 22:03:29 +0000

    b

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html", "./**/*.html"], // Alle HTML-Dateien scannen
+    theme: {
+        extend: {
+              colors: {
+                      primary: "#8e44ad",
+                              primaryHover: "#7d3c98",
+                                      secondary: "#ff6b6b",
+                                              text: "#ccc",
+                                                      background: "#1e1e2f",
+                                                              card: "#2a2a44",
+                                                                    },
+                                                                        },
+                                                                          },
+                                                                            plugins: [],
+                                                                            };
+                                                                            
+                            
